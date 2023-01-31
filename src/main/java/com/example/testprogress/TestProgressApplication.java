76ff89216681d2f5b3da2f0169af222f9d296774
ansehyeong <dkstpgud@gmail.com>
2023-01-31 20:34:46 +0900

    feat: initial commit for test

---
+++ src/main/java/com/example/testprogress/TestProgressApplication.java
@@ -0,0 +1,13 @@
+package com.example.testprogress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProgressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProgressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testprogress/TestProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testprogress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProgressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
